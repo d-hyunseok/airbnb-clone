35c74c4e964c4d0ebdc87309b3f087dc41ae860e
hyunseokkim <hangouts@kakao.com>
2021-04-17 13:55:18 +0900

    리뷰 어디민 / 모델 확장

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from core import models as core_models
+
+
+class Reviews(core_models.TimeStampedModel):
+
+    """reviews model definition"""
+
+    review = models.TextField()
+    accuracy = models.IntegerField()
+    communication = models.IntegerField()
+    cleanliness = models.IntegerField()
+    location = models.IntegerField()
+    check_in = models.IntegerField()
+    value = models.IntegerField()
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.review} - {self.room}'
