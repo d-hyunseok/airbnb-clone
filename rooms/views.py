c9d598e54864f7dc1b1cfc1aa30d43a5c9b1ce89
hyunseokkim <hangouts@kakao.com>
2021-04-21 23:35:47 +0900

    리스트뷰를 사용해 객실정보 띄우기

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from . import models
 
-# Create your views here.
+
+class HomeView(ListView):
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
